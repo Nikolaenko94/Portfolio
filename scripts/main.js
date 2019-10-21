fcf1e84eb652a27d8574d9a5331da97517865620
Stanislav Nikolaenko <grot_cs5@mail.ru>
2019-10-21 17:29:53 +0300

    add education box

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,11 @@
-// expansion panel
-let my_button = document.getElementById("educ_block"),
-    my_education_panel= document.getElementById("educ_panel"),
-    my_arrow = document.getElementById("my_arrow");
-
-function expansion_panel(){            
-   my_arrow.style.transform = "rotate(180deg)";  
-   my_education_panel.style.display="flex";    
+    // expansion panel
+function expansion_panel(){   
+   let display = document.getElementById("educ_panel").style.display;    
+   if(display=='none'){
+    document.getElementById("educ_panel").style.display="flex";
+    document.getElementById("my_arrow").style.transform = "rotate(180deg)";   
+   }else{
+    document.getElementById("educ_panel").style.display="none";
+    document.getElementById("my_arrow").style.transform = "rotate(0deg)";   
+   }   
 }
